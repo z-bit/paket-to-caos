3c452e6bb9b584b3f55a1372f958661a44b2812a
Zoppelt Günther <sgz@schmolck.de>
2016-04-29 16:07:56 +0200

    add main.js

--- main.js
+++ main.js
@@ -1,17 +1,82 @@
 // main.js
-
 console.log("I am happy!");
+
+var chokidar = require('chokidar');
+
+var watcher = chokidar.watch('P:\_SPPS_PDF\*.pdf', {
+	// ignored: /[\/\\]\./,
+	persistent: true
+});
+
+var log = console.log.bind(console);
+
+watcher.on('add', function(path) {
+	log('File', path, 'has been added.');
+});
+
+/*
+require('chokidar').watch('.', {ignored: /[\/\\]\./}).on('all', function(event, path) {
+  console.log(event, path);
+});
+*/
+
+return true;
+
+function getPdf(listOfFiles) {
+	for (var i=0; i<listOfFiles.length; i++) {
+		if (listOfFiles[i].match(/pdf$/)) {
+			return listOfFiles[i];
+		} 
+	}	
+	return false;		
+}
+
+//todo: doc should be loaded without a name, the only pdf in folder 
+//var doc = process.argv[2];
+//var doc = 'no doc';
+var fs = require('fs');
+fs.readdir(process.cwd(), function(err, files) {
+	if (err) throw(err);
+	var doc = getPdf(files);
+	
+	var ptt = require('pdf-to-text');
+	var option = {from: 0, to: 10}; // from page 0 to 10
+	
+	ptt.pdfToText(doc, option, function(err, pdf) {
+		if (err) throw(err);
+	  var base = pdf.indexOf('\n');
+		var fin = pdf.substr(base+64, 17);
+		
+		base = pdf.indexOf('\n1');
+		nr = pdf.substr(base+3, 10);
+		bez = pdf.substr(base+14, 50).trim();
+		for (var i=0; i<bez.length; i++) {
+			if (bez.charCodeAt(i) == 65533) {
+				bez = bez.substr(0, i) +'ö'+  bez.substr(i+1);
+			}	
+		}
+		console.log(bez);
+		//chr = pdf.substr(base+19, 1);
+		//console.log(chr.charCodeAt(0));
+	});
+	
+});
+
+
+
+
 
 var ptt = require('pdf-to-text');
 var doc = process.argv[2];
 
 var option = {from: 0, to: 10}; // from page 0 to 10
 
+/*
 ptt.pdfToText(doc, option, function(err, data) {
 	if (err) throw(err);
 	console.log(data);
-};
-
+)};
+*/
 /*
 ptt.info(doc, function (err, info) {
 	if (err) throw(err);
